578b9ee0ba2cff8a50813331752c0808dc7873dc
Corax <Corax34@users.noreply.github.com>
2025-11-04 19:33:37 +0100

    Added class declarations

---
+++ src/FrogIT/Resource.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+enum class ResourceType
+{
+    TEXTURE,
+    ANIMATION,
+    SOUND,
+}
+
+class Resource
+{
+}
---
+++ src/FrogIT/SoundScape.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+class SoundScape
+{
+    void playSounds();
+}
