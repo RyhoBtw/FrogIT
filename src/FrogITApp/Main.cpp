35a5c5dba775422c9a76211d568852005c5bdae2
Corax34 <50240040+Corax34@users.noreply.github.com>
2025-11-11 11:53:01 +0100

    Enhance UI and add target-dependency for assets-copy

--- src/FrogITApp/Main.cpp
+++ src/FrogITApp/Main.cpp
@@ -10,6 +10,29 @@
 #pragma comment(lib, "Dwmapi.lib")
 #endif
 
+const int FRAME_TOP_BORDER_Y = 116;
+const int FRAME_LEFT_BORDER_X = 64;
+const int FRAME_INNER_X = 311;
+const int FRAME_INNER_Y = 441;
+
+void minimizeWindow(sf::RenderWindow &window)
+{
+#if defined(_WIN32)
+    HWND hwnd = window.getNativeHandle();
+    ShowWindow(hwnd, SW_MINIMIZE);
+#elif defined(__APPLE__)
+    void *nsWindow = (void *)window.getNativeHandle();
+    ((void (*)(id, SEL))objc_msgSend)((id)nsWindow, sel_getUid("miniaturize:"), nil);
+#elif defined(__linux__)
+    Display *display = XOpenDisplay(NULL);
+    if (!display) return;
+    ::Window win = window.getNativeHandle();
+    XIconifyWindow(display, win, DefaultScreen(display));
+    XFlush(display);
+    XCloseDisplay(display);
+#endif
+}
+
 int main()
 {
     // Load custom frame
@@ -24,7 +47,7 @@ int main()
     auto window =
         sf::RenderWindow(sf::VideoMode({ frameTexture.getSize().x, frameTexture.getSize().y }, desktop.bitsPerPixel),
             "FrogIT",
-            sf::Style::None// removes OS title bar and borders
+            sf::Style::None // removes OS title bar and borders
         );
     window.setFramerateLimit(60);
     if (!ImGui::SFML::Init(window)) return -1;
@@ -41,6 +64,7 @@ int main()
     sf::Clock clock;
     auto dragging = false;
     sf::Vector2i dragOffset = { 0, 0 };
+    float vol = 0.0f, freq = 0.0f;
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             ImGui::SFML::ProcessEvent(window, *event);
@@ -48,7 +72,7 @@ int main()
 
             // --- Start drag ---
             if (const auto *e = event->getIf<sf::Event::MouseButtonPressed>()) {
-                if (e->button == sf::Mouse::Button::Left) {
+                if (e->button == sf::Mouse::Button::Left && sf::Mouse::getPosition(window).y < 115) {
                     dragging = true;
                     dragOffset = sf::Mouse::getPosition(window);// position inside window
                 }
@@ -72,6 +96,34 @@ int main()
         window.clear(sf::Color::Transparent);
 
         // Content
+        bool open = true;
+        ImGui::SetNextWindowPos(ImVec2(FRAME_LEFT_BORDER_X, FRAME_TOP_BORDER_Y), ImGuiCond_Always);
+        ImGui::SetNextWindowSize(ImVec2(FRAME_INNER_X, FRAME_INNER_Y), ImGuiCond_Always);
+        ImGui::Begin(
+                "UI",
+                &open,
+                ImGuiWindowFlags_NoMove | 
+                ImGuiWindowFlags_NoResize | 
+                ImGuiWindowFlags_NoCollapse |
+                ImGuiWindowFlags_NoTitleBar);
+        ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 24);
+        ImGui::SliderFloat("Volume1", &vol, 0.0f, 10.0f);
+        ImGui::SliderFloat("Frequency1", &freq, 0.0f, 10.0f);
+
+        float windowWidth = ImGui::GetWindowSize().x;
+        float buttonWidth = 20.0f;
+        ImGui::SetCursorPos(ImVec2(windowWidth - (3 * buttonWidth) - ImGui::GetStyle().WindowPadding.x, ImGui::GetStyle().WindowPadding.y));
+        ImGui::PushStyleColor(ImGuiCol_Button, IM_COL32(200, 50, 50, 255));// red
+        if (ImGui::Button("-", ImVec2(buttonWidth, buttonWidth))) { 
+            minimizeWindow(window);
+        }
+        ImGui::SetCursorPos(ImVec2(windowWidth - buttonWidth - ImGui::GetStyle().WindowPadding.x, ImGui::GetStyle().WindowPadding.y));
+        if (ImGui::Button("X", ImVec2(buttonWidth, buttonWidth))) { 
+            window.close(); 
+        }
+        ImGui::PopStyleColor();
+
+        ImGui::End();
 
         window.draw(frameSprite);
 
