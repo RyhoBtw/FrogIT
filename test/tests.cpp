924c052badc72f552bb1f6b480b109eeb8673bc1
Corax34 <50240040+Corax34@users.noreply.github.com>
2026-05-05 16:36:00 +0200

    Added integration tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,8 +1,17 @@
 #include <catch2/catch_test_macros.hpp>
+#include "FrogApp.hpp"
 
-
-// TODO Add tests
-TEST_CASE("Frog Test 1")
+TEST_CASE("FrogApp can be instantiated", "[FrogApp]")
 {
+  FrogApp app;
+  // If we reach here without crashing, the test passes
   REQUIRE(true);
+}
+
+TEST_CASE("FrogApp window can be accessed", "[FrogApp]")
+{
+  FrogApp app;
+  const sf::RenderWindow& window = app.getWindow();
+  // Verify that we got a valid reference
+  REQUIRE(window.isOpen());
 }
